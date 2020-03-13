3c5f0da3f11728a47cbed2f76cf96b0da1adc8b7
dongun kim <>
2020-03-13 18:40:49 +0900

    - book page 91(create calculator).

--- capter3/calculator.go
+++ capter3/calculator.go
@@ -1,11 +1,43 @@
 package capter3
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	zeroUnicode = 48
+	nineUnicode = 57
+)
+
 // Calculator returns the evaluation result of the given expr.
 // The expression can hav +,-,*,/,(,) operators and decimal integers.
 // Operators and operands should be space delimited.
 func Calculator(expr string) int {
-	//var ops []string
-	//var nums []int
+	expr = strings.Replace(strings.TrimSpace(expr), " ", "", -1)
+	var ops []string
+	var nums []int
+	fmt.Println(expr)
+	fmt.Println(operators)
+
+	for i, r := range expr {
+		if r >= zeroUnicode && r <= nineUnicode { // number
+			n, _ := strconv.Atoi(string(r))
+			nums = append(nums, n)
+		} else { // others
+			if vo := isValidOperator(r); !vo {
+				panic("include invalid operator in expression")
+			} else {
+				ops = append(ops, string(r))
+				if i == 0 {
+					nums = append(nums, 0)
+				}
+			}
+		}
+	}
+	fmt.Println(ops)
+	fmt.Println(nums)
 	//// nums stack 에 있는 마지막 number 가지고 오고 pop
 	//pop := func() (last int) {
 	//	last = nums[len(nums)-1]
@@ -21,4 +53,14 @@ func Calculator(expr string) int {
 	//	}
 	//}
 	return 0
+}
+
+var operators = "+-*/()"
+func isValidOperator(o int32) bool {
+	for _, r := range operators {
+		if o == r {
+			return true
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/discoveryGo/capter3"
+)
 
 func main() {
-	src := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-	fmt.Println(src)
-	src = append(src[:4], src[3:]...)
-	src[4] = -1
-	fmt.Println(src)
+	capter3.Calculator(" -  11+1*3*(23+8/2)")
+	fmt.Println( -  11+1*3*(23+8/2))
 }
